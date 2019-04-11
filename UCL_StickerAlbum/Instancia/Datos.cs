326d28da38a948d97e1bebd7880e3e748d9b32fc
Lester Andrés García Aquino <lagarcia2015url@gmail.com>
2019-04-10 21:57:59 -0600

    Adding principal controllers
    Adding principal controllers

---
+++ UCL_StickerAlbum/Instancia/Datos.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using UCL_StickerAlbum.Models;
+
+namespace UCL_StickerAlbum.Instancia
+{
+    public class Datos
+    {
+        private static Datos _instance = null;
+
+        public static Datos Instance
+        {
+            get
+            {
+                if (_instance == null)
+                {
+                    _instance = new Datos();
+                }
+                return _instance;
+            }
+
+        }
+
+        public Album UCLAlbum = new Album();
+    }
+}
