2d7128eefa6898a20b217de47f1fe0e69e0fb42e
Lester Andrés García Aquino <lagarcia2015url@gmail.com>
2019-04-10 21:34:26 -0600

    Creating principal models & deleting .suo innecesary file
    Creating principal models & deleting .suo innecesary file

---
+++ UCL_StickerAlbum/Models/Equipo.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.ComponentModel.DataAnnotations;
+
+namespace UCL_StickerAlbum.Models
+{
+    public class Equipo : IComparable
+    {
+        //Nombre del equipo/club
+        [Display(Name = "Equipo")]
+        public string NombreEquipo { get; set; }
+        //Club
+        [Display(Name = "Club")]
+        public string Club { get; set; }
+        //Listado de jugadores
+        public List<Jugador> Jugadores { get; set; }
+        //Nombre de los stickers ya registrados
+        [Display(Name = "Registradas")]
+        public int Registradas { get; set; }
+        //Nombre de los stickers faltantes
+        [Display(Name = "Restantes")]
+        public int Restantes { get; set; }
+        //Se recibirán 22 jugadores en dicho listado por reglas de convocatorio de 2 equipo de 11 jugadores
+        public Equipo(string nombreEquipo, string club, List<int> jugadores)
+        {
+            NombreEquipo = nombreEquipo;
+            Club = club;
+            Jugadores = new List<Jugador>(22);
+
+            for (int i = 0; i < Jugadores.Capacity; i++)
+            {
+                var j = new Jugador(NombreEquipo + (i + 1));
+                Jugadores.Add(j);
+            }
+            foreach (var item in jugadores)
+            {
+                var nombre = NombreEquipo + item;
+
+                for (int i = 0; i < Jugadores.Count; i++)
+                {
+                    if (Jugadores[i].Nombre == nombre)
+                    {
+                        if (Jugadores[i].Registrada == true)
+                        {
+                            Jugadores[i].Repetida++;
+                        }
+                        else
+                        {
+                            Jugadores[i].Registrada = true;
+                        }
+
+                    }
+                }
+            }
+        }
+        //Calculo de los stickers ya registrados y los restantes, así como detectar si se obtiene un sticker que no se tenía
+        public void Calcular()
+        {
+            Registradas = 0;
+            Restantes = 0;
+            foreach (var item in Jugadores)
+            {
+                if (item.Registrada)
+                {
+                    Registradas++;
+                }
+                else
+                {
+                    Restantes++;
+                }
+            }
+        }
+        public int CompareTo(object obj)
+        {
+            var comparer = (Equipo)obj;
+            return NombreEquipo.CompareTo(comparer.NombreEquipo);
+        }
+    }
+}
---
+++ UCL_StickerAlbum/Models/Jugador.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace UCL_StickerAlbum.Models
+{
+    public class Jugador
+    {
+        public string Nombre { get; set; }
+        public bool Registrada { get; set; }
+        public int Repetida { get; set; }
+        //Seteo de valores a variables previamente declaradas
+        public Jugador(string nombre)
+        {
+            Nombre = nombre;
+            Registrada = false;
+            Repetida = 0;
+        }
+    }
+}
